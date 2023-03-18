b9d4c0a0394c23c880c2079fcd0e18a7591b4f34
Vladimir Dolgiy <vladimirdolgijv@gmail.com>
2023-03-18 20:38:23 +0300

    implement comment service

--- client/src/app/helper/error-interceptor.service.ts
+++ client/src/app/helper/error-interceptor.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import {TokenStorageService} from "../service/token-storage.service";
+import {NotificationService} from "../service/notification.service";
+import {HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpRequest} from "@angular/common/http";
+import {catchError, Observable, throwError} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorInterceptorService {
 
-  constructor() { }
+  constructor(private tokenService: TokenStorageService,
+              private notificationService: NotificationService) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(catchError(err => {
+      if (err.status === 401) {
+        this.tokenService.logOut();
+        window.location.reload();
+      }
+
+
+      const error = err.error.message || err.statusText;
+      this.notificationService.showSnackBar(error);
+      return throwError(error);
+    }));
+  }
 }
+
+export const authErrorInterceptorProviders = [
+  {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptorService, multi: true}
+];
