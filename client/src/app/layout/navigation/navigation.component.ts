dbb4b514187ef30ace8f166affd589275066f50c
Vladimir Dolgiy <vladimirdolgijv@gmail.com>
2023-03-19 17:25:25 +0300

    implement navigation component

---
+++ client/src/app/layout/navigation/navigation.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {TokenStorageService} from "../../service/token-storage.service";
+import {UserService} from "../../service/user.service";
+import {User} from "../../models/User";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent {
+  isLoggedIn = false;
+  isDataLoaded = false;
+  user: User;
+
+  constructor(private tokenService: TokenStorageService,
+              private userService: UserService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenService.getToken();
+
+    if(this.isLoggedIn) {
+      this.userService.getCurrentUser()
+        .subscribe(data => {
+          this.user = data;
+          this.isDataLoaded = true;
+        })
+    }
+  }
+
+  logout(): void {
+    this.tokenService.logOut();
+    this.router.navigate(['/login']);
+  }
+}
