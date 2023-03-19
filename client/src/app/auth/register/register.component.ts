c932e4f9a1e4bce5743746c9a67654b992da3fab
Vladimir Dolgiy <vladimirdolgijv@gmail.com>
2023-03-19 17:14:21 +0300

    implement registration component

--- client/src/app/auth/register/register.component.ts
+++ client/src/app/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../service/auth.service";
+import {NotificationService} from "../../service/notification.service";
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  public registerForm: FormGroup;
 
+  constructor(
+    private authService: AuthService,
+    private notificationService: NotificationService,
+    private fb: FormBuilder
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.registerForm = this.createRegisterForm();
+  }
+
+  createRegisterForm(): FormGroup {
+    return this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      username: ['', Validators.compose([Validators.required])],
+      firstname: ['', Validators.compose([Validators.required])],
+      lastname: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])],
+      confirmPassword: ['', Validators.compose([Validators.required])],
+    });
+  }
+
+  submit(): void {
+    console.log(this.registerForm.value);
+
+    this.authService.register({
+      email: this.registerForm.value.email,
+      username: this.registerForm.value.username,
+      firstname: this.registerForm.value.firstname,
+      lastname: this.registerForm.value.lastname,
+      password: this.registerForm.value.password,
+      confirmPassword: this.registerForm.value.confirmPassword,
+    }).subscribe(data => {
+      console.log(data);
+      this.notificationService.showSnackBar('Successfully Registered!');
+    }, error => {
+      this.notificationService.showSnackBar('Something went wrong during registration');
+    });
+  }
 }
